0487bf3a7d2c56b64377532cd2f8c823296c8025
valterj <56932864+valterj@users.noreply.github.com>
2020-10-28 12:11:40 -0300

    Implementação banco de dados

--- 01 RestWithSpringBootUdemy Scaffold Eclipse/src/main/java/br/com/erudio/controller/PersonController.java
+++ 01 RestWithSpringBootUdemy Scaffold Eclipse/src/main/java/br/com/erudio/controller/PersonController.java
@@ -4,7 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,41 +20,33 @@ import br.com.erudio.services.PersonService;
 @RestController
 @RequestMapping("/person")
 public class PersonController {
-	
+
 	@Autowired
 	private PersonService service;
-	
-	@RequestMapping(method = RequestMethod.POST,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@GetMapping
+	public List<Person> findAll() {
+		return this.service.findAll();
+	}
+
+	@GetMapping("/{id}")
+	public Person findById(@PathVariable("id") Long id) {
+		return this.service.findById(id);
+	}
+
+	@PostMapping
 	public Person create(@RequestBody Person person) {
 		return this.service.create(person);
 	}
-	
-	@RequestMapping(method = RequestMethod.PUT,
-			consumes = MediaType.APPLICATION_JSON_VALUE,
-			produces = MediaType.APPLICATION_JSON_VALUE)
+
+	@PutMapping
 	public Person update(@RequestBody Person person) {
-		return this.service.create(person);
+		return this.service.update(person);
 	}
-	
-	@RequestMapping(value = "/{id}",
-			method = RequestMethod.DELETE)
-	public void delete(@PathVariable("id") String id) {
-		this.service.delete(id);		
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable("id") Long id) {
+		this.service.delete(id);
 	}
-	
-	@RequestMapping(method = RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll() {
-		return this.service.findAll();		
-	}
-	
-	@RequestMapping(value = "/{id}",
-			method = RequestMethod.GET,
-			produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person findById(@PathVariable("id") String id) {
-		return this.service.findById(id);		
-	}	
 
 }
--- 01 RestWithSpringBootUdemy Scaffold Eclipse/src/main/java/br/com/erudio/services/PersonService.java
+++ 01 RestWithSpringBootUdemy Scaffold Eclipse/src/main/java/br/com/erudio/services/PersonService.java
@@ -1,59 +1,47 @@
 package br.com.erudio.services;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.erudio.exception.ResourceNotFoundException;
 import br.com.erudio.model.Person;
+import br.com.erudio.repository.PersonRepository;
 
 @Service
 public class PersonService {
-	
-	private final AtomicLong counter = new AtomicLong();
-	
+
+	@Autowired
+	PersonRepository repository;
+
 	public Person create(Person person) {
-		return person;
-	}
-	
-	public Person update(Person person) {
-		return person;
-	}
-	
-	public void delete(String id) {
-		
-	}
-	
-	public List<Person> findAll() {
-		List<Person> persons = new ArrayList<Person>();
-		
-		for (int i = 0; i < 8; i++) {
-			Person person = mockPerson(i);
-			persons.add(person);
-		}
-		
-		return persons ;
-	}
-	
-	public Person findById(String id) {
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Valter");
-		person.setLastName("Zimmermann");
-		person.setAddress("Joinville, SC");
-		person.setGender("M");
-		return person ;
+		return repository.save(person);
 	}
 
-	private Person mockPerson(int i) {
-		Person person = new Person();
-		person.setId(counter.incrementAndGet());
-		person.setFirstName("Valter" + i);
-		person.setLastName("Zimmermann" + i);
-		person.setAddress("Joinville, SC" + i);
-		person.setGender("M");
-		return person ;
+	public Person update(Person person) {
+		Person entity = this.findById(person.getId());
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		return repository.save(entity);
+	}
+
+	public Person findById(Long id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No record found for this id"));
+	}
+
+	public List<Person> findAll() {
+		return repository.findAll();
+	}
+	
+	public void delete(Long id) {
+		Person entity = this.findById(id);
+		repository.delete(entity);
 	}
 
 }
